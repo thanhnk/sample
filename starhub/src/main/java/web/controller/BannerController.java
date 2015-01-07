5a91492bb2b5facb5b7067c0cbc748c4e80498ec
Thanh Ky <thanhnk00269@gmail.com>
2015-01-08 02:02:13 +0700

    finish banner section.

--- starhub/src/main/java/web/controller/BannerController.java
+++ starhub/src/main/java/web/controller/BannerController.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -37,20 +38,27 @@ public class BannerController {
 	@Autowired
 	private BannerService bannerService;
 
+	@PreAuthorize("isAuthenticated()")
 	@RequestMapping(value = "/{id}", method = RequestMethod.POST)
-	public String update(@Valid Banner banner, BindingResult bindingResult, Model uiModel,
-			HttpServletRequest httpServletRequest, RedirectAttributes redirectAttributes, Locale locale,
-			@RequestParam(value = "file", required = false) Part file, @PathVariable("id") Long id) {
+	public String update(@Valid Banner banner, BindingResult bindingResult,
+			Model uiModel, HttpServletRequest httpServletRequest,
+			RedirectAttributes redirectAttributes, Locale locale,
+			@RequestParam(value = "file", required = false) Part file,
+			@PathVariable("id") Long id) {
 		logger.info("Updating banner");
 		if (bindingResult.hasErrors()) {
-			uiModel.addAttribute("message",
-					new Message("error", messageSource.getMessage("banner_save_fail", new Object[] {}, locale)));
+			uiModel.addAttribute(
+					"message",
+					new Message("error", messageSource.getMessage(
+							"banner_save_fail", new Object[] {}, locale)));
 			uiModel.addAttribute("banner", banner);
 			return "banners/update";
 		}
 		uiModel.asMap().clear();
-		redirectAttributes.addFlashAttribute("message",
-				new Message("success", messageSource.getMessage("banner_save_success", new Object[] {}, locale)));
+		redirectAttributes.addFlashAttribute(
+				"message",
+				new Message("success", messageSource.getMessage(
+						"banner_save_success", new Object[] {}, locale)));
 		// Process upload file
 		if (file != null && file.getSize() > 0) {
 			banner.setPhoto(FileUtil.getFileContent(file));
@@ -62,9 +70,12 @@ public class BannerController {
 		}
 		bannerService.save(banner);
 		logger.info("Banner saved");
-		return "redirect:/banners/" + UrlUtil.encodeUrlPathSegment(banner.getId().toString(), httpServletRequest);
+		return "redirect:/banners/"
+				+ UrlUtil.encodeUrlPathSegment(banner.getId().toString(),
+						httpServletRequest);
 	}
 
+	@PreAuthorize("isAuthenticated()")
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public String updateForm(@PathVariable("id") Long id, Model uiModel) {
 		uiModel.addAttribute("banner", bannerService.findById(id));
